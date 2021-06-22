a99cdfa40d77a90eceb4b9de3106a31caef37e49
tonytit <20162912@student.hust.edu.vn>
2021-06-22 16:07:41 +0700

    Done add mail

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Hello From FastShare
+
+Thank you so much for subscribing to our sharing system.
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
---
+++ resources/views/user/dynamic_email_template.blade.php
@@ -0,0 +1,2 @@
+<p>Hello {{auth()->user()->name}}</p>
+<p>Thank you so much for supporting FastShare Systems</p>
